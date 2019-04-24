251af0aff9eba28de61bd7c2d82439f859df68cd
Magiczne <michalkamilkleszczynski@gmail.com>
2019-04-24 16:25:46 +0200

    Dokumentacja

--- lib/Generator.py
+++ lib/Generator.py
@@ -27,51 +27,127 @@ class Generator:
     # region Setters
 
     def set_metre(self, n: int, m: int):
+        """
+        Set metre
+
+        Args:
+            n:  Number of notes
+            m:  Note rytmical value
+        """
         self.metre = (n, m)
         return self
 
     def set_bar_count(self, bar_count: int):
+        """
+        Set bar count
+
+        Args:
+            bar_count:  Bar count
+        """
         # TODO: Set bar count
         return self
 
     def set_shortest_note_duration(self, duration: int):
+        """
+        Set shortest note duration
+
+        Args:
+            duration:   Shortest note duration
+        """
         # TODO: Set
         return self
 
     def set_start_note(self, note: Note):
+        """
+        Set note on which script will start generating notes
+
+        Args:
+            note:   Starting note
+        """
         # TODO: Set & Walidacja czy poprawna nuta
         return self
 
     def set_end_note(self, note: Note):
+        """
+        Set note on which script will finish generating notes
+
+        Args:
+            note:   Finishing note
+        """
         # TODO: Set & Walidacja czy poprawna nuta
         return self
 
     def set_ambitus(self, lowest: Optional[Note], highest: Optional[Note]):
+        """
+        Set ambitus
+
+        Args:
+             lowest:    Optional value for lowest note
+             highest:   Optional value for highest note
+        """
         # TODO: Sprawdzenie czy lowest, highest przy podaniu jest instancją Note
         # TODO: Ustawienie ambitusu, może opcjonalne parametry, aby ustawiać tylko dół albo tylko górę
         return self
 
     def set_interval_probability(self, interval: str, probability: float):
+        """
+        Set probability for specified interval
+
+        Args:
+            interval:       Interval name
+            probability:    Probability for specified interval
+        """
         # TODO: Ustawienie prawdopodobieństwa wystąpienia konkretnego interwału
         return self
 
     def set_intervals_probability(self, probabilities: List[float]):
+        """
+        Set probabilities for all intervals at once
+
+        Args:
+            probabilities:  List of probabilities. All values should be summed to 1
+        """
         # TODO: Walidacja czy podano odpowiednio długą listę i czy suma wartości równa się 1
         # TODO: Ustawienie wszystkich prawdopodobieństw na raz
         return self
 
     # endregion
 
-    def split_to_bars(self, notes: List[Note]):
+    def split_to_bars(self, notes: List[Note]) -> List[List[Note]]:
+        """
+        Split given list of notes into bars. Notes that overlap between bars will be split
+        and then connected with a ligature.
+
+        Args:
+            notes:  List of notes
+
+        Returns:
+            List of bars of notes
+        """
         # TODO: Sprawdzenie czy parametr notes jest listą nut
         # TODO: Implementacja pogrupowania nut w takty, złamanie odpowiednich nut łukiem, etc...
         pass
 
-    def group_bars(self, bars: List[List[Note]]):
+    def group_bars(self, bars: List[List[Note]]) -> List[List[Note]]:
+        """
+        Perform grouping on each bar of notes according to the musical grouping rules
+
+        Args:
+            bars:   List of bars of notes
+
+        Returns:
+            Grouped list of bars of notes
+        """
         # TODO: Implementacja grupowania nut wewnątrz taktów według zasad grupowania zależnie od metrum
         pass
 
-    def generate(self):
+    def generate(self) -> List[Note]:
+        """
+        Generate list of notes based on previously set parameters.
+
+        Returns:
+            List of generated notes
+        """
         # TODO: Generacja nut na podstawie parametrów podanych wsześniej przez użytkownika
         # TODO: Przekazanie wygenerowanej listy nut do funkcji dzieljącej na takty (split_to_bars)
         # TODO: Przekazanie pogrupowanej listy taktów do funkcji grupującej nuty wewnątrz taktów (group_in_bars)
--- lib/Writer.py
+++ lib/Writer.py
@@ -14,23 +14,51 @@ class Writer:
     # region Data appending utils
 
     def blank(self):
+        """Add blank line to the output file"""
         self.lines.append('')
 
     def line(self, data: str, indent: int = 0):
+        """
+        Add line with content to the output data
+
+        Args:
+            data:   Line data
+            indent: Number of tabs indentation
+        """
         indent_str = self.get_indent(indent)
         self.lines.append('{}{}'.format(indent_str, data))
 
     def command(self, data: str, indent: int = 0):
+        """
+        Add command line to the output data (starting with \ character)
+
+        Args:
+             data:      Command data
+             indent:    Number of tabs indentation
+        """
         indent_str = self.get_indent(indent)
         self.lines.append('{}\\{}'.format(indent_str, data))
 
     def block_start(self, name: Optional[str], indent: int = 0):
+        """
+        Add block start with optional block name to the output data
+
+        Args:
+            name:   Optional block name
+            indent: Number of tabs indentation
+        """
         if name is None:
             self.line('{', indent)
         else:
             self.command('{} {{'.format(name), indent)
 
     def block_end(self, indent: int = 0):
+        """
+        Add block end to the output data
+
+        Args:
+             indent:    Number of tabs indentation
+        """
         self.line('}', indent)
 
     # endregion
@@ -39,10 +67,28 @@ class Writer:
 
     @staticmethod
     def get_indent(indent: int) -> str:
+        """
+        Get indentation in string format
+
+        Args:
+            indent:     Number of tabs indentation
+
+        Returns:
+            String with number of specified tabs
+        """
         return '' if indent <= 0 else '\t' * indent
 
     @staticmethod
-    def get_bar(bar_type: BarType):
+    def get_bar(bar_type: BarType) -> str:
+        """
+        Get string value for bar based on type
+
+        Args:
+             bar_type:  Bar type
+
+        Returns:
+            String representation of bar type
+        """
         return '\\bar "{}"'.format(bar_type.value)
 
     # endregion
@@ -50,6 +96,13 @@ class Writer:
     # region Lilypond file parts
 
     def header(self, paper_size: str = 'a4', show_bar_numbers: bool = True):
+        """
+        Add typical lilypond header to the output data. You can specify paper size and disable bar numbers
+
+        Args:
+            paper_size:         Paper size
+            show_bar_numbers:   Determine if bar numbers should be shown
+        """
         self.command('version "2.18.2"')
         self.blank()
         self.block_start('paper')
@@ -69,9 +122,25 @@ class Writer:
             self.blank()
 
     def time_signature(self, n: int, m: int, indent: int = 0):
+        """
+        Add time signature to the output data
+
+        Args:
+            n:          Number of notes
+            m:          Note rytmical value
+            indent:     Number of tabs indentation
+        """
         self.command('time {}/{}'.format(n, m), indent)
 
     def key_signature(self, key: str, key_type: KeyType, indent: int = 0):
+        """
+        Add key signature to the output data
+
+        Args:
+            key:        Key primary note
+            key_type:   Key type (minor/major)
+            indent:     Number of tabs indentation
+        """
         self.command('key {} \\{}'.format(key, key_type.value), indent)
 
     # endregion
@@ -79,16 +148,24 @@ class Writer:
     # region File operations
 
     def export(self):
+        """Export lilypond file"""
         content: str = '\n'.join(self.lines)
         f = open('output/source/{}.ly'.format(self.filename), 'w+')
         f.write(content)
         f.close()
 
     def compile(self):
+        """Compile lilypond file to pdf"""
         os.system('lilypond -o output/compiled output/source/{}.ly'.format(self.filename))
 
     # endregion
 
     def parse(self, bars: List[List[Note]]):
+        """
+        Parse list of bars and place it into the output data.
+
+        Args:
+            bars:   List of bars containing notes to parse into output data
+        """
         # TODO: Parsowanie listy nut i wrzucanie ich do listy self.data
         pass
